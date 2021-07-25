5eb4a6ccf1f7c88394dacf28bf0327bd2bc589fe
Marco Siracusano <msiracusano@Mac-mini-de-Marco.local>
2021-07-25 09:18:48 -0300

    Removing Storyboard and creating ViewController programmatically.

--- Intercorp Test/ViewController.swift
+++ Intercorp Test/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var titleLabel: UILabel!
+
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        titleLabel = createTitleLabel("Creación de Cliente")
+        
+        view.addSubview(titleLabel)
+        
+        setTitleLabelConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
-
+    
+    
+    private func createTitleLabel(_ text: String) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.font = .boldSystemFont(ofSize: 25)
+        label.textColor = .black
+        
+        return label
+    }
+    
+    private func setTitleLabelConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+    }
 }
 
